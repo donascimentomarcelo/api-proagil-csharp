10e22f867012dfeea651c7b586a248101fcbcf67
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-07-29 23:03:56 -0300

    Creating Event Model

---
+++ Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.WebAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Place { get; set; }
+        public string EventDate { get; set; }
+        public string Theme { get; set; }
+        public int QtdPeople { get; set; }
+        public string Allotment { get; set; }
+    }
+}
