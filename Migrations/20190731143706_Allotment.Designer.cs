3452d48a1ab88149dc18fa60f51b0b6c0dd1e7b8
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-08-01 22:06:15 -0300

    Changing database to MySql

--- Migrations/20190731143706_Allotment.Designer.cs
+++ Migrations/20190731143706_Allotment.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProAgil.WebAPI.Data;
 
 namespace ProAgil.WebAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190731143706_Allotment")]
+    partial class Allotment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
