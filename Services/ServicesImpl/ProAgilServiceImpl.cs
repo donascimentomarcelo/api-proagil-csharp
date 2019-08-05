3493ec9a4ab790df14f58ce9f060eeb96511e1f3
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-08-04 21:44:42 -0300

    Configuring NoTracking in context

--- Services/ServicesImpl/ProAgilServiceImpl.cs
+++ Services/ServicesImpl/ProAgilServiceImpl.cs
@@ -12,6 +12,7 @@ namespace ProAgil.WebAPI.Services.ServicesImpl
         public ProAgilServiceImpl(DataContext context)
         {
             _context = context;
+            _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
         public void Add<T>(T entity) where T : class
         {
