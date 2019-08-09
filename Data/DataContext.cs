e6bafbee93220a85dfa6d9be52ed9572c0cd0370
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-08-08 21:27:32 -0300

    Creating the entities for configure roles

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProAgil.WebAPI.Models;
 
 namespace ProAgil.WebAPI.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options)
         {
