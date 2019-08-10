24c102abee79005bd80649236fe318385f9b966c
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-08-10 17:56:51 -0300

    Creating migrations to identity and recreating database

--- Identity/User.cs
+++ Identity/User.cs
@@ -6,7 +6,6 @@ namespace ProAgil.WebAPI.Identity
 {
     public class User : IdentityUser<int>
     {
-        [Column(TypeName="nvarcher(150)")]
         public string FullName { get; set; }
         public List<UserRole> UserRoles { get; set; }
     }
